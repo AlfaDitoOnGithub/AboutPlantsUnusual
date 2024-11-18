984ecf11eb0c93ac237e4536e161d87e57a9288c
AlfaDitoOnGithub <alfaditoauliadenova@gmail.com>
2024-11-18 21:20:14 +0700

    level 1 event done

--- Assets/Scripts/Runtime/Level1Event.cs
+++ Assets/Scripts/Runtime/Level1Event.cs
@@ -1,18 +1,58 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Level1Event : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+   [SerializeField] private bool leverActivate = false;
+   [SerializeField] private GameObject uiTextPrompt;
+   [SerializeField] private GameObject gerbang;
+   public UnityEvent gateOpen;
 
-    // Update is called once per frame
-    void Update()
-    {
+   public void OnTriggerEnter2D(Collider2D other)
+   {
+       if (other.CompareTag("Player"))
+       {
+        leverActivate = true;
+        uiTextPrompt.SetActive(true);
+        Debug.Log("Player Masuk");
+       }
+   }
+
+   public void OnTriggerExit2D(Collider2D other)
+   {
+       if (other.CompareTag("Player"))
+       {
+        leverActivate = false;
+        Debug.Log("Player Keluar");
+        uiTextPrompt.SetActive(false);
+       }
+   }
+
+   /// <summary>
+   /// Update is called every frame, if the MonoBehaviour is enabled.
+   /// </summary>
+   void Update()
+   {
+       if (leverActivate && Input.GetKeyDown(KeyCode.F)){
+        Debug.Log("posisi gerbang besi pertama=" + gerbang.transform.position);
+        Interact();
+        Debug.Log("Tombol dipencet");
         
-    }
+       }
+   }
+   public void Interact(){
+
+
+    gateOpen.Invoke();
+    Debug.Log("Gerbang dibuka");
+    gerbang.transform.Translate(0.0f, 4.0f, 0.0f);
+    Debug.Log("posisi gerbang besi kedua=" + gerbang.transform.position);
+        
+
+
+   }
+
 }
