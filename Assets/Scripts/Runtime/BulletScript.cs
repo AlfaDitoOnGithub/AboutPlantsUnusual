c2ca92117da3952056e41a41ee8307ccbe6aaaf6
Alfadito AD <89778375+AlfaDitoOnGithub@users.noreply.github.com>
2024-11-30 21:22:25 +0700

    some adjustment

--- Assets/Scripts/Runtime/BulletScript.cs
+++ Assets/Scripts/Runtime/BulletScript.cs
@@ -6,17 +6,18 @@ using CodeMonkey.Utils;
 public class BulletScript : MonoBehaviour
 {
    private Vector3 shootDir;
+   [SerializeField] private float moveSpeed = 50f;
   
    
    public void Setup(Vector3 shootDir){
     this.shootDir = shootDir;
-    Destroy(gameObject, 1f);
+    Destroy(gameObject, 3f);
    }
 
    
    private void Update()
    {
-       float moveSpeed = 10f;
+    //    float moveSpeed = 50f;
        transform.position += shootDir * moveSpeed * Time.deltaTime;
        
    }
--- Assets/Scripts/Runtime/EnemyHitbox.cs
+++ Assets/Scripts/Runtime/EnemyHitbox.cs
@@ -9,6 +9,7 @@ public class EnemyHitbox : MonoBehaviour {
 private static List<EnemyHitbox> enemyList;
 private Animator spriteAnimator;
 [SerializeField] private int health;
+
     private void Awake()
     {
         if (enemyList == null){enemyList = new List<EnemyHitbox>();};
@@ -17,7 +18,7 @@ private Animator spriteAnimator;
     }
     public void Damage() {
         //enemy hurt animation is trigger
-        spriteAnimator.SetTrigger("Hurt");
+        spriteAnimator.SetTrigger("Damage");
         health-=1;
         if(health <= 0){
             Destroy(gameObject);
