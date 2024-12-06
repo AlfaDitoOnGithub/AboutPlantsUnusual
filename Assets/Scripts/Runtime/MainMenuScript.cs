44df9079dddae53a04ea4a13b07c6592ded70192
Alfadito AD <89778375+AlfaDitoOnGithub@users.noreply.github.com>
2024-12-06 14:13:17 +0700

    add more functionality
    next Add the carousel function

--- Assets/Scripts/Runtime/MainMenuScript.cs
+++ Assets/Scripts/Runtime/MainMenuScript.cs
@@ -4,15 +4,8 @@ using UnityEngine;
 
 public class MainMenuScript : MonoBehaviour
 {
-    public GameObject MainMenu;
-    public GameObject CreditsMenu;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        MainMenuButton();
-    }
-
+        
+    
     public void PlayNowButton()
     {
         // Play Now Button has been pressed, here you can initialize your game (For example Load a Scene called GameLevel etc.)
@@ -22,15 +15,23 @@ public class MainMenuScript : MonoBehaviour
     public void CreditsButton()
     {
         // Show Credits Menu
-        MainMenu.SetActive(false);
-        CreditsMenu.SetActive(true);
+        // MainMenu.SetActive(false);
+        // CreditsMenu.SetActive(true);
     }
 
     public void MainMenuButton()
     {
-        // Show Main Menu
-        MainMenu.SetActive(true);
-        CreditsMenu.SetActive(false);
+         // Show Main Menu
+        // MainMenu.SetActive(true);
+        // CreditsMenu.SetActive(false);
+    }
+
+    public void BackButton(){
+        SoundManager.PlaySound(SoundType.MenuBack,0.8f);
+    }
+
+    public void ClickedButton(){
+        SoundManager.PlaySound(SoundType.MenuInteract,0.7f);
     }
 
     public void QuitButton()
